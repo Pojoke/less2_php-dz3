9b58a23c43331432f2afced912fe434a72596cab
Pojoke <pavlykdmutro@gmail.com>
2024-09-13 12:11:42 +0300

    comit

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 const p=3.14;
-$r= rand( 25, 500);
+$r= rand( min: 25, max: 500);
 $s= p*$r;
-$d = $r*2;
+$d = $r*2 ;
 echo"$s";
 
-echo "<div style='width: $d  px; height: $d px; background: blue; border-radius: 50%;'></div>";
+echo "<div style='width: $d  ; height: $d ; background: blue; border-radius: 50%;'></div>";
 
 ?>;
